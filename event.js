e348927ccc4a817a3faaccfd4a1595f09a9fd551
xibxor <admin@scrollto.com>
2018-08-22 00:07:25 -0700

    all

---
+++ event.js
@@ -0,0 +1,13 @@
+const commands = {};
+
+// commands.add = function(name, date){
+
+// };
+// commands.add.desc = [
+// 	"adds an event to the calendar",
+// 	"name date"
+// ];
+
+// commands.remove.desc = ["adds an event to the calendar"];
+
+module.exports = commands;
---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+const modules = {};
+["event", "emoji"].forEach(
+  v => modules[v] = require(`./${v}.js`)
+);
+
+["ready", "guildCreate", "guildDelete"].forEach(
+  v => client.on(v, () => {
+  	//todo
+  })
+)
+
+client.on("message", async message => {
+  if (message.author.bot || message.author.id == client.user.id) return;
+  if (!/^!/.test(message.content)) return;
+  
+  const args = message.content.slice(1).trim().split(/\s+/g);
+  const command = args.shift().toLowerCase().split(".");
+  const handler = modules[command[0]];
+
+  if (!handler) return;
+
+  await handler[command[1] || ""].apply({client: client, message: message}, args);
+});
+
+client.login(config.token);
+
