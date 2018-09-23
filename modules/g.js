96d8098b1c32158ebd146b6315c0db34a7788176
Shawn Presser <shawnpresser@gmail.com>
2018-09-23 06:24:40 -0500

    all

--- modules/g.js
+++ modules/g.js
@@ -10,7 +10,7 @@ async function img(s){
 	},
 	headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.16 Safari/537.36'}});
 
-	const firstResult = data.match(/"ou":"(.+?)","ow":/)[1];
+	const firstResult = (data.match(/"ou":"(.+?)","ow":/) || [])[1];
 
 	if (!firstResult){
 		await this.channel.send(`*${this.author} Sorry, no results found!*`);
