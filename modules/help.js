3dec357f2b5553c3865b14ec91c06bfbf087e187
xibxor <admin@scrollto.com>
2018-09-24 08:13:08 -0700

    all

--- modules/help.js
+++ modules/help.js
@@ -3,17 +3,16 @@ const request = require('request-promise-native');
 async function help(s) {
   await this.channel.send(`
   commands:
-
+    !color <color> - change your user color
+    !role.add <roleName> <color> - add a role to yourself
     !g.img <foo> -- google image search for foo
     !yt <foo> -- youtube results for foo
     !wiki <foo> -- wikipedia results for foo
 
   more commands:
-    chess
     color
     latex
     pfp
-    role
     timer
   `);
 }
